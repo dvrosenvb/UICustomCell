704627ced7c4e19de1cf0bcb0483c220cd0add64
Rosendo <62037216+dvrosenvb@users.noreply.github.com>
2023-05-12 18:10:21 -0600

    Initial Commit

---
+++ UICustomCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICustomCell
+//
+//  Created by Rosendo Vazquez on 12/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
